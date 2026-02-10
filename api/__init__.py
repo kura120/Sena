dd7771832380ec6cf6a91315f14c3e783112b464
santi <89491373+kura120@users.noreply.github.com>
2026-02-10 08:50:31 -0500

    Delete api directory

--- api/__init__.py
+++
@@ -1,6 +0,0 @@
-# src/api/__init__.py
-"""API module for Sena."""
-
-from src.api.server import app, start_server
-
-__all__ = ["app", "start_server"]
--- api/websocket/__init__.py
+++
@@ -1,6 +0,0 @@
-# src/api/websocket/__init__.py
-"""WebSocket module."""
-
-from src.api.websocket.manager import WebSocketManager, ws_manager
-
-__all__ = ["WebSocketManager", "ws_manager"]
